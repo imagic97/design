c9805ea8ee9892614726b61079a39c3935ed4647
imagic97 <315636696@qq.com>
2020-04-01 17:54:44 +0800

    添加axios开发环境配置

--- ebook-vue/src/api/api.js
+++ ebook-vue/src/api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-// export const API_TO_GET_VIEW = "/read/view?file=";
-// export const API_TO_GET_COVER = "/read/cover?file=";
-// export const API_TO_GET_CONTENT = "/read/content?file=";
-////获取书籍资源，封面，图片，章节
+axios.defaults.timeout = 5000; // 请求超时
+axios.defaults.baseURL = "/api";
+
+// 获取书籍资源，封面，图片，章节
 export function getResource(file, href) {
   return axios({
     method: "GET",
@@ -13,7 +13,7 @@ export function getResource(file, href) {
     }
   });
 }
-
+// 目录
 export function getContent(file) {
   return axios({
     method: "GET",
@@ -23,7 +23,7 @@ export function getContent(file) {
     }
   });
 }
-
+// 章节样式
 export function getChapterCSS(file) {
   return axios({
     method: "GET",
@@ -34,15 +34,7 @@ export function getChapterCSS(file) {
   });
 }
 
-// http://localhost:8080/user/getUserSelf
-export function getUserSelf() {
-  return axios({
-    method: "GET",
-    url: "/user/getUserSelf"
-  });
-}
-
-///user/login?userName=test&password=123456
+// 登录
 export function login(userName, password) {
   return axios({
     method: "POST",
@@ -53,13 +45,14 @@ export function login(userName, password) {
     }
   });
 }
+// 登出
 export function logout() {
   return axios({
     method: "GET",
     url: "/user/logout"
   });
 }
-///user/register?userName=user&password=123456&email=
+// 注册
 export function register(userName, password, email) {
   return axios({
     method: "POST",
@@ -71,6 +64,7 @@ export function register(userName, password, email) {
     }
   });
 }
+// 用户修改
 export function modifyUser(userName, password, email) {
   return axios({
     method: "POST",
@@ -79,6 +73,47 @@ export function modifyUser(userName, password, email) {
       userName: userName,
       password: password,
       email: email
+    }
+  });
+}
+// 书架列表
+export function getUserSelf() {
+  return axios({
+    method: "GET",
+    url: "/user/getUserSelf"
+  });
+}
+
+//移出书架
+export function deleteBookFromSelf(selfId) {
+  return axios({
+    method: "GET",
+    url: "/user/deleteBookFromSelf",
+    params: {
+      selfId: selfId
+    }
+  });
+}
+// 上传电子书
+export function uploadBook(file) {
+  let param = new FormData();
+  param.append("multipartFile", file);
+  return axios({
+    headers: { "Content-Type": "multipart/form-data" },
+    method: "POST",
+    url: "/book/upload",
+    data: param
+  });
+}
+
+// 删除电子书
+export function deleteBook(bookId, fileName) {
+  return axios({
+    method: "GET",
+    url: "/book/delete",
+    params: {
+      bookId: bookId,
+      fileName: fileName
     }
   });
 }
