48d77d417bef9907018d2d02c8491c7d5d063b45
imagic97 <315636696@qq.com>
2020-04-04 20:29:17 +0800

    C:/Program Files/Git/

--- ebook-vue/src/components/common/me.vue
+++ ebook-vue/src/components/common/me.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="about_me" style="text-align:center">
     <h2>关于</h2>
-    <div>
-      WEBOOK，沧海中一粒ePub阅读小站，愿你在书海中遨游~
-    </div>
+    <div>WEBOOK，沧海中一粒ePub阅读小站，愿你在书海中遨游~</div>
     <div>作者：imagic97</div>
     <div>email：315636696@qq.com</div>
+    <button @click="clearCaChe()">清空缓存</button>
   </div>
 </template>
+<script>
+import lS from "@/util/localStorage";
+export default {
+  methods: {
+    clearCaChe() {
+      lS.clear();
+    }
+  }
+};
+</script>
